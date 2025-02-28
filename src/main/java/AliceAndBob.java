f970b34cdd237fdb2d33b46ba796ccf4f0190baf
Nathan <natefintak@gmail.com>
2025-02-28 18:48:04 -0500

    in progress

---
+++ src/main/java/AliceAndBob.java
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+public class AliceAndBob {
+    public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	String person1 = "Alice";
+	String person2 = "Bob";
+
+	System.out.println("Your name: ");
+	
+	String name = String.toString(scan.nextLine());
+	
+    }
+}
