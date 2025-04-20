337964594d780de467f2fed684c2a269d0b1f0b7
Nathan <natefintak@gmail.com>
2025-04-20 17:36:46 -0400

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
@@ -6,18 +8,24 @@ public class Main {
 
     public static void main(String[] args ){
         // create a new object of class Main
-
+        Main main = new Main();
+        main.compute();
         // call the compute() method on that new object
     }
 
     public void compute() {
         // Get input string of name from user
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Your name: ");
+        String name = scan.nextLine();
         // Create conditional to check name against Alice and Bob
-
-        // Print greeting to screen if Alice or Bob are true
-        
-        // Print "You are neither Alice nor Bob." otherwise
+        if (name.equals("Alice") || name.equals("Bob")) {
+            // Print greeting to screen if Alice or Bob are true
+            System.out.println("Hello " + name + "!");
+        } else {
+            // Print "You are neither Alice nor Bob." otherwise
+            System.out.println("You are neither Alice nor Bob.");
+        }
 
     }
 }
